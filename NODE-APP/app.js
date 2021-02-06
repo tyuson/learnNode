b956674db53917025b8b0bb291d7bafd4e5965a1
tyuson <g.tyuson@gmail.com>
2021-02-06 23:17:21 +0900

    HTMLで日本語を表示する

--- NODE-APP/app.js
+++ NODE-APP/app.js
@@ -1,6 +1,16 @@
 const http = require('http');
 
 let server = http.createServer(
-    (req, res) => res.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>')
+    (req, res) => {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<!DOCTYPE html><html lang="ja">');
+        res.write('<head><meta charset="utf-8">');
+        res.write('<title>Hello</title></head>');
+        res.write('<body><h1>Hello</h1>');
+        res.write('<p>This is Node.js sample page.</p>');
+        res.write('<p>これは、Node.jsのサンプルページです。</p>');
+        res.write('</body></html>');
+        res.end();
+    }
 );
 server.listen(3000);
