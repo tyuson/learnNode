8c81bc01a196c098c80bee3c93949b7ccec30d58
tyuson <g.tyuson@gmail.com>
2021-02-06 23:29:50 +0900

    ファイルを読み込んで表示する

--- NODE-APP/app.js
+++ NODE-APP/app.js
@@ -1,16 +1,15 @@
 const http = require('http');
+const fs = require('fs');
 
 let server = http.createServer(
     (req, res) => {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<!DOCTYPE html><html lang="ja">');
-        res.write('<head><meta charset="utf-8">');
-        res.write('<title>Hello</title></head>');
-        res.write('<body><h1>Hello</h1>');
-        res.write('<p>This is Node.js sample page.</p>');
-        res.write('<p>これは、Node.jsのサンプルページです。</p>');
-        res.write('</body></html>');
-        res.end();
+        fs.readFile('./index.html', 'UTF-8',
+            (error, data) => {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            }
+        );
     }
 );
 server.listen(3000);
