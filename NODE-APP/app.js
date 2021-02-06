ea4d3d2baddf058871352fe599e638fdfb3bc256
tyuson <g.tyuson@gmail.com>
2021-02-06 23:54:02 +0900

    テンプレートエンジン EJS を使う

--- NODE-APP/app.js
+++ NODE-APP/app.js
@@ -1,15 +1,16 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
 let server = http.createServer(getFromClient);
 server.listen(3000);
 console.log('Server started!');
 
 function getFromClient(req, res) {
-    fs.readFile('./index.html', 'UTF-8',
-        (error, data) => {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            res.end();
-        }
-    );
+    let content = ejs.render(index_page);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(content);
+    res.end();
 }
+
