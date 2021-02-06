ab45ab778791ff376188b346a90f068aabe22578
tyuson <g.tyuson@gmail.com>
2021-02-06 23:35:38 +0900

    関数を切り分ける

--- NODE-APP/app.js
+++ NODE-APP/app.js
@@ -1,15 +1,15 @@
 const http = require('http');
 const fs = require('fs');
-
-let server = http.createServer(
-    (req, res) => {
-        fs.readFile('./index.html', 'UTF-8',
-            (error, data) => {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                res.end();
-            }
-        );
-    }
-);
+let server = http.createServer(getFromClient);
 server.listen(3000);
+console.log('Server started!');
+
+function getFromClient(req, res) {
+    fs.readFile('./index.html', 'UTF-8',
+        (error, data) => {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        }
+    );
+}
