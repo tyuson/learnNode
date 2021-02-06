af8b556839238b7a752b11e3bb318e23d83ecdfc
tyuson <g.tyuson@gmail.com>
2021-02-06 23:10:38 +0900

    HTMLを出力

--- NODE-APP/app.js
+++ NODE-APP/app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 let server = http.createServer(
-    (req, res) => res.end('Hello Node.js')
+    (req, res) => res.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>')
 );
 server.listen(3000);
